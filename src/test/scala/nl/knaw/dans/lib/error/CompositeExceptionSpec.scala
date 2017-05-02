9f7ff637451db9c15acdf14cb12d9392b1dbe597
Richard van Heest <richard.van.heest@dans.knaw.nl>
2017-05-02 12:02:50 +0200

    license headers and mvn build fix

--- src/test/scala/nl/knaw/dans/lib/error/CompositeExceptionSpec.scala
+++ src/test/scala/nl/knaw/dans/lib/error/CompositeExceptionSpec.scala
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2016 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package nl.knaw.dans.lib.error
 
 import java.io.{ PrintWriter, StringWriter }
@@ -28,10 +43,6 @@ class CompositeExceptionSpec extends FlatSpec with Matchers {
     val ce1 = CompositeException(ex4, ex5)
     val ce2 = CompositeException(ce1, ex6)
     ce2.getMessage shouldBe "3 exceptions occurred."
-  }
-
-  private def traverseCauses(ex: Throwable): Seq[Throwable] = {
-    Stream.iterate(ex)(_.getCause).takeWhile(null !=)
   }
 
   "getCause" should "return a chain of causes" in {
